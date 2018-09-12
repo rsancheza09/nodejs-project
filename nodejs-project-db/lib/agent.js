9c84f3337623194f247381485332f9a935676c43
Randall Sánchez <rasanchez@omnicomhealthgroup.com>
2018-09-12 12:42:41 -0600

    add findById implementation and test

--- nodejs-project-db/lib/agent.js
+++ nodejs-project-db/lib/agent.js
@@ -1,7 +1,9 @@
 'use strict'
 
 module.exports = function setupAgentService (AgentModel) {
-  function findById (id) {}
+  function findById (id) {
+    return AgentModel.findById(id)
+  }
 
   return {
     findById

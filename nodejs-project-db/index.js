94f07021ff8fac77a5b4bb6f778accb93fbce87c
Randall Sánchez <rasanchez@omnicomhealthgroup.com>
2018-09-12 09:25:25 -0600

    add basic database module implementation

--- nodejs-project-db/index.js
+++ nodejs-project-db/index.js
@@ -1,6 +1,20 @@
 'use strict'
 
+const setupDatabase = require('./lib/db')
+const setupAgentModel = require('./modules/agent')
+const setupMetricModel = require('./modules/metric')
+
 module.exports = async function (config) {
+  const sequelize = setupDatabase(config)
+  const AgentModel = setupAgentModel(config)
+  const MetricModel = setupMetricModel(config)
+
+  // Database Relations
+  AgentModel.hasMany(MetricModel) // 0 -> *
+  MetricModel.belongsTo(AgentModel) // 1 <-> 1
+
+  await sequelize.authenticate()
+
   const Agent = {}
   const Metric = {}
   return {

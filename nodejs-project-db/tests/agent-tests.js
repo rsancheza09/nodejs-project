ba206eb5b89e6aac5c51175f26999318234748a3
Randall Sánchez <rasanchez@omnicomhealthgroup.com>
2018-09-12 11:43:40 -0600

    add mocks and stubs with sinon

--- nodejs-project-db/tests/agent-tests.js
+++ nodejs-project-db/tests/agent-tests.js
@@ -1,18 +1,46 @@
 'use strict'
 
 const test = require('ava')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
 
 let config = {
   logging: function () {}
 }
 
+let MetricStub = {
+  belongsTo: sinon.spy()
+}
+
+let AgentStub = null
 let db = null
+let sandbox = null
 
 test.beforeEach(async () => {
-  const setupDatabase = require('../')
+  sandbox = sinon.createSandbox()
+  AgentStub = {
+    hasMany: sinon.spy()
+  }
+  const setupDatabase = proxyquire('../', {
+    './modules/agent': () => AgentStub,
+    './modules/metric': () => MetricStub
+  })
+
   db = await setupDatabase(config)
+})
+
+test.afterEach(t => {
+  sandbox && sandbox.restore()
 })
 
 test('Agent', t => {
   t.truthy(db.Agent, 'Agent service should exist')
+})
+
+// When we have stubs we have to use serial
+test.serial('Setup DB', t => {
+  t.true(AgentStub.hasMany.called, 'AgentModel.hasMany was executed')
+  t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argument should be the MetricModel')
+  t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo was executed')
+  t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument should be the AgentModel')
 })
